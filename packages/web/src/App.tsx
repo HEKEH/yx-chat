269c0ddf2adc1ace7e5f18eb9d755e7fc493173f
何凯 <hekai.gustav@bytedance.com>
2023-06-17 16:06:36 +0800

    chore: 添加eslint

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { CSSProperties, defineComponent, PropType, ref } from "vue";
-import 'element-plus/dist/index.css';
+import "element-plus/dist/index.css";
 import { HelloWorld } from "./components/HelloWorld";
 import s from "./App.module.sass";
 import { fetch } from "./infrastructure/socketIO/fetch";
