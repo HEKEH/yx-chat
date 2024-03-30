e685f65de44927275c4a8a4d4ef349c8e9d672a1
何凯 <hekai.gustav@bytedance.com>
2024-03-30 11:51:02 +0800

    chore: optimize App.tsx

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -4,37 +4,72 @@ import { defineComponent, onBeforeUnmount, onErrorCaptured, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { HomePage } from '~/components/homepage';
 import { SocketEventType, SocketIO } from '~/infra/socket-io';
-import { initI18n } from './infra/i18n';
+import { initI18n as _initI18n } from './infra/i18n';
 import { provideGlobalStore } from './utils/vue';
 
-function addSocketEventListeners(socketIO: SocketIO) {
-  const { t } = useI18n();
-  const onSocketConnectError = () => {
-    ElNotification.error({
-      message: t('server.connectError'),
-    });
-  };
-
-  const onSocketDisconnect = (reason: string) => {
-    ElNotification.error({
-      message: `${t('server.disconnect')}: ${t(reason)}`,
-    });
-  };
-
-  const subscriptions = [
-    socketIO.addSocketEventListener(
-      SocketEventType.connectError,
-      onSocketConnectError,
-    ),
-    socketIO.addSocketEventListener(
-      SocketEventType.disconnect,
-      onSocketDisconnect,
-    ),
-  ];
-
-  onBeforeUnmount(() => {
-    subscriptions.forEach(subscription => subscription.unsubscribe());
+function initI18n() {
+  const isI18nReady = ref(false);
+  _initI18n().then(() => {
+    isI18nReady.value = true;
   });
+  const { t } = useI18n();
+  return {
+    isI18nReady,
+    t,
+  };
+}
+
+function initSocketIO() {
+  const addSocketEventListeners = (_socketIO: SocketIO) => {
+    const { t } = useI18n();
+    const onSocketConnectError = () => {
+      ElNotification.error({
+        message: t('server.connectError'),
+      });
+    };
+
+    const onSocketDisconnect = (reason: string) => {
+      ElNotification.error({
+        message: `${t('server.disconnect')}: ${t(reason)}`,
+      });
+    };
+
+    const subscriptions = [
+      _socketIO.addSocketEventListener(
+        SocketEventType.connectError,
+        onSocketConnectError,
+      ),
+      _socketIO.addSocketEventListener(
+        SocketEventType.disconnect,
+        onSocketDisconnect,
+      ),
+    ];
+
+    onBeforeUnmount(() => {
+      subscriptions.forEach(subscription => subscription.unsubscribe());
+    });
+  };
+  const socketIO = SocketIO.instance;
+  addSocketEventListeners(socketIO);
+  socketIO.connect(); // socket连接
+  onBeforeUnmount(() => {
+    // socket断开连接
+    socketIO.disconnect();
+  });
+}
+
+function initGlobalStore() {
+  const isGlobalStoreReady = ref(false);
+  const globalStore = provideGlobalStore();
+  // 尝试根据token登录
+  globalStore.loginByToken().finally(() => {
+    // 不管成不成功，都进入主页
+    isGlobalStoreReady.value = true;
+  });
+  return {
+    isGlobalStoreReady,
+    globalStore,
+  };
 }
 
 export default defineComponent({
@@ -48,33 +83,17 @@ export default defineComponent({
       return false;
     });
 
-    const isI18nReady = ref(false);
-    initI18n().then(() => {
-      isI18nReady.value = true;
-    });
-    const { t } = useI18n();
+    const { isI18nReady, t } = initI18n();
 
-    const socketIO = SocketIO.instance;
-    addSocketEventListeners(socketIO);
-    socketIO.connect(); // socket连接
-    onBeforeUnmount(() => {
-      // socket断开连接
-      socketIO.disconnect();
-    });
+    initSocketIO();
 
-    const isReady = ref(false);
-    const globalStore = provideGlobalStore();
-    // 尝试根据token登录
-    globalStore.loginByToken().finally(() => {
-      // 不管成不成功，都进入主页
-      isReady.value = true;
-    });
+    const { isGlobalStoreReady } = initGlobalStore();
 
     return () => {
       if (!isI18nReady.value) {
         return null;
       }
-      if (!isReady.value) {
+      if (!isGlobalStoreReady.value) {
         // loading
         return (
           <div
