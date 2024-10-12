7167b03021f96fa04322eabecdccd382f1b3c3b8
Will Ho <kai.h@decode-group.com>
2024-10-12 14:49:40 +0800

    feat: optimize server docker file

--- scripts/docker/server/build-docker-image.sh
+++ scripts/docker/server/build-docker-image.sh
@@ -1,104 +1,114 @@
 #!/bin/bash
-IMAGE_NAME="yx-chat-server"
-ERROR=""
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-PROJECT_ROOT="${SCRIPT_DIR}/../../../"
-cd "${PROJECT_ROOT}" # assure current dir is the exact project root directory
+IMAGE_NAME="yx-chat-server"
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 LOG_FILE=${SCRIPT_DIR}/build-local-image.log
 
-# # get version from package.json
+PROJECT_ROOT="${SCRIPT_DIR}/../../../"
+cd "${PROJECT_ROOT}" # assure current dir is the exact project root directory
+
+PORT=""
+
+# get version from package.json
 # VERSION=$(node -p "require('./packages/server/package.json').version")
 # IMAGE_TAG="${IMAGE_NAME}:${VERSION}"
 
-PORT=""
-function load_port_from_env_file() {
-  local env_file="./.env.production"
-  local port_var_name="PUBLIC_SERVER_PORT"
-
-  if [ ! -f "$env_file" ]; then
-    echo "Error: $env_file file not found" >&2
-    return 1
-  fi
-
-  PORT=$(grep -E "^[[:space:]]*$port_var_name[[:space:]]*=" "$env_file" | sed -E "s/^[[:space:]]*$port_var_name[[:space:]]*=[[:space:]]*[\"']?([^\"']+)[\"']?[[:space:]]*$/\1/" | tr -d '\n\r')
-
-  if [ -z "$PORT" ]; then
-    echo "Error: $port_var_name not found in $env_file" >&2
-    return 1
-  fi
-
-  return 0
-
-  #     local env_file="$1"
-  #     if [ -f "$env_file" ]; then
-  #         while IFS= read -r line || [[ -n "$line" ]]; do
-  #             # 去除行首尾的空白字符
-  #             line=$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-
-  #             # 忽略注释和空行
-  #             if [[ ! $line =~ ^\# && -n $line ]]; then
-  #                 # 提取键和值
-  #                 key=$(echo "$line" | cut -d '=' -f 1)
-  #                 value=$(echo "$line" | cut -d '=' -f 2-)
-
-  #                 # 去除值两端的引号
-  #                 value=$(echo "$value" | sed -e 's/^["\x27]//' -e 's/["\x27]$//')
-
-  #                 # 导出变量
-  #                 if [[ -n "$key" && -n "$value" && "$key" = 'PUBLIC_SERVER_PORT' ]]; then
-  #                     PORT=$value
-  #                     return 0
-  #                 fi
-  #             fi
-  #         done < "$env_file"
-  #     else
-  #         echo "$env_file file not found"
-  #         return 1
-  #     fi d
+function log() {
+  echo "$1" | tee -a ${LOG_FILE}
 }
 
-if ! load_port_from_env_file; then
-  exit 1
-fi
+function log_error() {
+  echo "Error: $1" | tee -a "${LOG_FILE}" >&2
+}
+
+function load_port_from_env_file() {
+  local ENV_FILE="./.env.production"
+  local PORT_VAR_NAME="PUBLIC_SERVER_PORT"
+
+  if [ ! -f "$ENV_FILE" ]; then
+    log_error "$ENV_FILE file not found"
+    exit 1
+  fi
+
+  PORT=$(grep -E "^[[:space:]]*$PORT_VAR_NAME[[:space:]]*=" "$ENV_FILE" | sed -E "s/^[[:space:]]*$PORT_VAR_NAME[[:space:]]*=[[:space:]]*[\"']?([^\"']+)[\"']?[[:space:]]*$/\1/" | tr -d '\n\r')
+
+  if [ -z "$PORT" ]; then
+    log_error "$PORT_VAR_NAME not found in $ENV_FILE"
+    exit 1
+  fi
+
+  # local ENV_FILE="$1"
+  # if [ -f "$ENV_FILE" ]; then
+  #   while IFS= read -r line || [[ -n "$line" ]]; do
+  #     # 去除行首尾的空白字符
+  #     line=$(echo "$line" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  #     # 忽略注释和空行
+  #     if [[ ! $line =~ ^\# && -n $line ]]; then
+  #       # 提取键和值
+  #       key=$(echo "$line" | cut -d '=' -f 1)
+  #       value=$(echo "$line" | cut -d '=' -f 2-)
+
+  #       # 去除值两端的引号
+  #       value=$(echo "$value" | sed -e 's/^["\x27]//' -e 's/["\x27]$//')
+
+  #       # 导出变量
+  #       if [[ -n "$key" && -n "$value" && "$key" = 'PUBLIC_SERVER_PORT' ]]; then
+  #         PORT=$value
+  #         return 0
+  #       fi
+  #     fi
+  #   done <"$ENV_FILE"
+  # else
+  #   echo "$ENV_FILE file not found"
+  #   return 1
+  # fi
+}
+
+function remove_image() {
+  log "Info: Removing existing image"
+  docker rmi "${IMAGE_NAME}" >/dev/null 2>&1
+}
+
+function build_image() {
+  log "Info: Building docker image"
+  if docker build . -f "${SCRIPT_DIR}/Dockerfile" -t "${IMAGE_NAME}" --build-arg PORT="${PORT}" >>${LOG_FILE} 2>&1; then
+    log "Docker image '${IMAGE_NAME}' built successfully."
+  else
+    log_error "build failed, Check ${LOG_FILE} for more details"
+    exit 1
+  fi
+}
 
 function stop_and_remove_container() {
-  # Stop and remove the existing container
+  log "Info: Stopping and removing existing container"
   docker stop ${IMAGE_NAME} >/dev/null 2>&1
   docker rm ${IMAGE_NAME} >/dev/null 2>&1
 }
 
-function remove_image() {
-  # Remove the existing image
-  docker rmi ${IMAGE_NAME} >/dev/null 2>&1
-}
-
-function build_image() {
-  # build docker
-  docker build . -f "${SCRIPT_DIR}/Dockerfile" --build-arg PORT="${PORT}" -t "${IMAGE_NAME}" || ERROR="build_image failed"
-}
-
-function log_message() {
-  if [[ ${ERROR} != "" ]]; then
-    echo >&2 "build failed, Please check build-local-docker-image.log for more details"
-    echo >&2 "ERROR: ${ERROR}"
-    exit 1
+function run_container() {
+  log "Info: Running new container"
+  if docker run -d -p "${PORT}:${PORT}" --name ${IMAGE_NAME} ${IMAGE_NAME} >>${LOG_FILE} 2>&1; then
+    log "Container started successfully."
+    log "To see container logs, use: docker logs ${IMAGE_NAME}"
   else
-    echo "docker image with tag '${IMAGE_NAME}' built sussessfully. Use below sample command to run the container"
-    echo "docker run -d -p ${PORT}:${PORT} --name ${IMAGE_NAME} ${IMAGE_NAME}"
-    echo ""
-    echo "Use below sample command to see logs"
-    echo "docker logs ${IMAGE_NAME}"
+    log_error "Failed to start the container. Check ${LOG_FILE} for details."
+    exit 1
   fi
 }
 
-echo "Info: Stopping and removing existing container and image" | tee ${LOG_FILE}
-stop_and_remove_container
-remove_image
+function main() {
+  : >"${LOG_FILE}"
+  load_port_from_env_file
+  remove_image
+  build_image
 
-if [[ ${ERROR} == "" ]]; then
-  echo "Info: Building docker image" | tee -a ${LOG_FILE}
-  build_image 1>>${LOG_FILE} 2>>${LOG_FILE}
-fi
+  # if yout only want to build image, then no need to run the following commands
+  stop_and_remove_container
+  run_container
 
-log_message | tee -a ${LOG_FILE}
+  log "All mission successfully completed. See ${LOG_FILE} for logs."
+}
+
+main "$@" # call main function with all arguments
--- scripts/docker/web/build-docker-image.sh
+++ scripts/docker/web/build-docker-image.sh
@@ -2,60 +2,62 @@
 
 IMAGE_NAME="yx-chat-web"
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-PROJECT_ROOT="${SCRIPT_DIR}/../../../"
-cd "${PROJECT_ROOT}" # assure current dir is the exact project root directory
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 LOG_FILE="${SCRIPT_DIR}/build-local-image.log"
 
-if [ -f "${LOG_FILE}" ]; then
-  rm "${LOG_FILE}"
-fi
+PROJECT_ROOT="${SCRIPT_DIR}/../../../"
+cd "${PROJECT_ROOT}" # assure current dir is the exact project root directory
+
 function log() {
-  echo "$1" | tee -a ${LOG_FILE}
+  echo "$1" | tee -a "${LOG_FILE}"
+}
+
+function log_error() {
+  echo "Error: $1" | tee -a "${LOG_FILE}" >&2
 }
 
 function remove_image() {
-  log "Info: Stopping and removing existing image"
-  # Remove the existing image
-  docker rmi ${IMAGE_NAME} >/dev/null 2>&1
+  log "Info: Removing existing image"
+  docker rmi "${IMAGE_NAME}" >/dev/null 2>&1
 }
 
 function build_image() {
   log "Info: Building docker image"
-  # build docker
-  docker build . -f "${SCRIPT_DIR}/Dockerfile" -t "${IMAGE_NAME}" 1>>${LOG_FILE} 2>>${LOG_FILE}
-  if [ $? -eq 0 ]; then
-    log "docker image with tag '${IMAGE_NAME}' built sussessfully."
+  if docker build . -f "${SCRIPT_DIR}/Dockerfile" -t "${IMAGE_NAME}" >>${LOG_FILE} 2>&1; then
+    log "Docker image '${IMAGE_NAME}' built successfully."
   else
-    echo >&2 "build failed, Please check build-local-image.log for more details"
+    log_error "build failed, Please check ${LOG_FILE} for more details"
     exit 1
   fi
 }
 
 function stop_and_remove_container() {
   log "Info: Stopping and removing existing container"
-  # Stop and remove the existing container
   docker stop ${IMAGE_NAME} >/dev/null 2>&1
   docker rm ${IMAGE_NAME} >/dev/null 2>&1
 }
 
 function run_container() {
   log "Info: Running new container"
-  docker run -d -p 8010:8080 --name ${IMAGE_NAME} ${IMAGE_NAME} 1>>${LOG_FILE} 2>>${LOG_FILE}
-  if [ $? -eq 0 ]; then
+  if docker run -d -p 8010:8080 --name ${IMAGE_NAME} ${IMAGE_NAME} >>${LOG_FILE} 2>&1; then
     log "Container started successfully."
-    log "Use below sample command to see container logs"
-    log "docker logs ${IMAGE_NAME}"
+    log "To see container logs, use: docker logs ${IMAGE_NAME}"
   else
-    log "Failed to start the container. Please check the logs for details."
+    log_error "Failed to start the container. Check ${LOG_FILE} for details."
     exit 1
   fi
 }
 
-remove_image
+function main() {
+  : >"${LOG_FILE}"
+  remove_image
+  build_image
 
-build_image
+  # if yout only want to build image, then no need to run the following commands
+  stop_and_remove_container
+  run_container
 
-stop_and_remove_container
+  log "All mission successfully completed. See ${LOG_FILE} for logs."
+}
 
-run_container
+main "$@" # call main function with all arguments
