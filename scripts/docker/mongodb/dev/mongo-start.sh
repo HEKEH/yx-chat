190d3419465cb9075bd156226989a00ddebf072e
Will Ho <kai.h@decode-group.com>
2024-10-12 09:48:08 +0800

    feat: update mongodb docker scripts

--- scripts/docker/mongodb/dev/mongo-start.sh
+++ scripts/docker/mongodb/dev/mongo-start.sh
@@ -3,14 +3,17 @@
 cd "$(dirname "$0")"
 
 create_volume_if_not_exists() {
-    local volume_name=$1
-    if ! docker volume inspect $volume_name > /dev/null 2>&1; then
-        echo "Creating volume: $volume_name"
-        docker volume create $volume_name
-    else
-        echo "Volume $volume_name already exists"
-    fi
+  local volume_name=$1
+  if ! docker volume inspect $volume_name >/dev/null 2>&1; then
+    echo "Creating volume: $volume_name"
+    docker volume create $volume_name
+  else
+    echo "Volume $volume_name already exists"
+  fi
 }
+
+export MONGO_PORT=27019
+export CONTAINER_NAME=yx-chat-mongodb-dev
 
 # docker network create mongo-network
 create_volume_if_not_exists mongo-data-dev
---
+++ scripts/docker/mongodb/prod/mongo-init.demo.js
@@ -0,0 +1,33 @@
+/** TODO: In production environment, copy this file as mongo-init.js and modify user & password */
+
+/* eslint-disable no-undef */
+
+function createUser(userName, password, dbName, role) {
+  try {
+    db = db.getSiblingDB(dbName);
+    const user = db.getUser(userName);
+    if (!user) {
+      db.createUser({
+        user: userName,
+        pwd: password,
+        roles: [{ role: role, db: dbName }],
+      });
+      print(`User ${userName} created successfully for database ${dbName}`);
+    } else {
+      print(`User ${userName} already exists for database ${dbName}`);
+    }
+  } catch (error) {
+    print(`An error occurred: ${error.message}`);
+    quit(1);
+  }
+}
+
+const userName = 'yxAdmin';
+const password = '111111';
+const dbName = 'yx-chat';
+createUser(userName, password, dbName, 'readWrite');
+
+/** TODO: Uncomment this if you want to create a global user for all databases */
+// const globalUserName = 'adminUser';
+// const globalPassword = 'adminPassword';
+// createUser(globalUserName, globalPassword, 'admin', 'userAdminAnyDatabase');
