055f227b2508de95ec029e2a2309f8e1edbc80b0
Will Ho <kai.h@decode-group.com>
2024-10-27 20:51:03 +0800

    feat: update element-plus

--- apps/web/src/index.ts
+++ apps/web/src/index.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import Element from 'element-plus';
+import { vLoading } from 'element-plus';
 import App from './App';
 import i18n from './infra/i18n';
 import './style.css';
@@ -7,8 +7,7 @@ import './styles/global.sass';
 import './styles/theme.sass';
 
 const app = createApp(App);
-app.use(Element); // TODO 最好干掉，不要全局如此使用
-// app.directive('v-loading', vLoading); // TODO 不生效，过一段时间再尝试
+app.directive('loading', vLoading);
 
 app.use(i18n);
 app.mount('#app');
